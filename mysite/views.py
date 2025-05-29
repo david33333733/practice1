6a007c9b1df0772d5e46b0fe0a1ced40c0ecea33
david oh <ghkvud5614@gmail.com>
2025-05-29 17:45:23 +0900

    마지막 링크거는거까지 했습니다

--- mysite/views.py
+++ mysite/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>메인페이지 입니다.</h1")
+    html="""
+    <h1>메인페이지 입니다.</h1>\n \
+    <p><a href="/apage/">apage로 이동합니다.</a></p>
+    <a href="/bpage/">bpage로 이동합니다.</a>
+    <a href="/cpage/">cpage로 이동합니다.</a>
+    """
+    return HttpResponse(html)
